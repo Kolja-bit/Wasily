0d87090f178f17d33327df8785988ff024343f40
Wasilii Martynov <nishilov@yandex.ru>
2025-02-02 17:01:50 +0300

    init11

--- src/main/java/searchengine/services/LemmasIndexingServiceImpl.java
+++ src/main/java/searchengine/services/LemmasIndexingServiceImpl.java
@@ -6,20 +6,17 @@ import org.apache.lucene.morphology.LuceneMorphology;
 import org.apache.lucene.morphology.english.EnglishLuceneMorphology;
 import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
 import org.jsoup.Jsoup;
-import org.springframework.stereotype.Service;
+import searchengine.model.IndexModel;
 import searchengine.model.LemmaModel;
 import searchengine.model.PageModel;
-import searchengine.model.IndexModel;
 import searchengine.model.SitesModel;
 import searchengine.repositories.IndexRepository;
 import searchengine.repositories.LemmasRepository;
-
 
 import java.io.IOException;
 import java.util.*;
 
 @Slf4j
-//@Service
 @RequiredArgsConstructor
 public class LemmasIndexingServiceImpl implements LemmasIndexingService {
     private final String stringHtml;
@@ -28,15 +25,15 @@ public class LemmasIndexingServiceImpl implements LemmasIndexingService {
     private final LemmasRepository lemmasRepository;
     private final IndexRepository indexRepository;
     private LuceneMorphology luceneMorph =null;
+
+
     private final static Set<String> RUSSIAN_REGEX=Set.of("ПРЕДЛ","СОЮЗ","МЕЖД","МС-П","МС");
     private final static Set<String> ENGLICH_REGEX=Set.of("ADJECTIVE","ARTICLE","PN_ADJ","PREP");
 
     public HashMap<String,Integer> getMapLemmas(){
-        String[]strings=getWebPageContent().replaceAll("[^A-Za-zА-Яа-я]+"," ")
-                .trim().split("\\s");
         HashMap<String,Integer> mapLemma=new HashMap<>();
-        for (String string:strings){
-            String str=string.toLowerCase();
+        List<String> list1=getListContext();
+        for (String str:list1){
             getLuceneMorph(str);
             List<String> list = luceneMorph.getMorphInfo(str);
             if (!RUSSIAN_REGEX.contains(list.get(0).split("\\s")[1])){
@@ -69,26 +66,42 @@ public class LemmasIndexingServiceImpl implements LemmasIndexingService {
         return luceneMorph;
     }
     public void recordLemmas(){
-        //HashSet<Integer> setLemmaId=new HashSet<>();
+            HashMap<String, Integer> hashMap = getMapLemmas();
+            for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+                LemmaModel lemma = new LemmaModel();
 
-        HashMap<String,Integer>hashMap=getMapLemmas();
-        for (Map.Entry<String,Integer>entry: hashMap.entrySet()){
-        LemmaModel lemma=new LemmaModel();
-        lemma.setSite(sites);
-        lemma.setLemma(entry.getKey());
-        lemma.setFrequency(2);//то число просто так
-        lemmasRepository.save(lemma);
+                lemma.setSite(sites);
+                lemma.setLemma(entry.getKey());
+                //int frequencyLemma=;
+                lemma.setFrequency(2);//то число просто так
+                IndexModel index = new IndexModel();
+                index.setLemma(lemma);
+                index.setPage(page);
+                index.setRank(Float.valueOf((entry.getValue())));
+                indexRepository.save(index);
+                lemmasRepository.save(lemma);
 
-        //setLemmaId.add(lemma.getId());
 
-        IndexModel index=new IndexModel();
+        /*IndexModel index=new IndexModel();
         index.setLemma(lemma);
         index.setPage(page);
         index.setRank(Float.valueOf((entry.getValue())));
-        indexRepository.save(index);
-        }
+        indexRepository.save(index);*/
+            }
 
         //log.info(String.valueOf(setLemmaId));
+    }
+    public List<String> getListContext(){
+        List<String> stringList=new ArrayList<>();
+        String[]strings=getWebPageContent().replaceAll("[^A-Za-zА-Яа-яЁё]+"," ")
+                .trim().split("\\s");
+        for (String string:strings) {
+            if (string.matches("[А-Яа-яЁё&&[^A-Za-z]]+") || string.matches("[A-Za-z&&[^А-Яа-яЁё]]+")) {
+                String str1 = string.toLowerCase();
+                stringList.add(str1);
+            }
+        }
+        return stringList;
     }
 
 }
